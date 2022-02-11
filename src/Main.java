484d504c972d87d640c4539e8154f89653d4a502
Mckelvin Ofosu-Frimpong <mickeyfrimpong@dhcp-172-30-238-96.guest.uc.edu>
2022-02-11 11:34:17 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    public static void main(String[] args)
+    {
+	int numToDefine = 1;
+
+    if (numToDefine % 2 == 0)
+    {
+        System.out.println("This is an even number");
+    } else
+    {
+        System.out.println("This is an Odd number");
+    }
+
+    }
+}
